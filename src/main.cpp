f44c3f44d1aa2bc919c77893b3add44e51e7c51a
Aditya Singh <Aditya.singh_cs.aiml24@gla.ac.in>
2026-02-20 09:01:13 +0530

    Refactor comments and define IR sensor pin
    Updated comments and defined IR sensor pin.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,10 @@
 #include <Arduino.h>
-/**
- * @file main.cpp
- * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
- *
- * @details
- * Reads digital input from IR sensor
- * and detects obstacle presence.
- */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
+// TODO 1: Define IR sensor digital pin (Use pin 2)
+const int IR_PIN = 2;
 
- // TODO 2:
- // Create variable to store sensor state
+// TODO 2: Create variable to store sensor state
+int irState = LOW;
 
 void setup() {
 
