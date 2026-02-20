54b71374333165b84ec5a258ef76b912983134ff
Aditya Singh <Aditya.singh_cs.aiml24@gla.ac.in>
2026-02-20 09:02:30 +0530

    Update TODO comments and print initialization messages

--- src/main.cpp
+++ src/main.cpp
@@ -11,11 +11,14 @@ void setup() {
     // TODO 3: Initialize Serial communication (9600 baud rate)
     Serial.begin(9600);
     
-    // TODO 4:
-    // Configure IR pin as INPUT
+    // TODO 4: Configure IR pin as INPUT
+    pinMode(IR_PIN, INPUT);
 
-    // TODO 5:
-    // Print system initialization message
+    // TODO 5: Print system initialization message
+    Serial.println("======================================");
+    Serial.println(" Embedded Obstacle Detection System ");
+    Serial.println("======================================");
+    Serial.println("Monitoring IR sensor on Pin 2...");
 }
 
 void loop() {
